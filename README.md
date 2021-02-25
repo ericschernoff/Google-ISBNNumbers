45f0368dc73dc1d5c256e311106c7c0dd042b58e
Eric Chernoff <eric@pepper.weaverstreet.net>
2021-02-25 14:51:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Google::ISBNNumbers - It's new $module
+
+# SYNOPSIS
+
+    use Google::ISBNNumbers;
+
+# DESCRIPTION
+
+Google::ISBNNumbers is ...
+
+# LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Eric Chernoff <eric@pepper.weaverstreet.net>
---
+++ lib/Google/ISBNNumbers.pm
@@ -0,0 +1,39 @@
+package Google::ISBNNumbers;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::ISBNNumbers - It's new $module
+
+=head1 SYNOPSIS
+
+    use Google::ISBNNumbers;
+
+=head1 DESCRIPTION
+
+Google::ISBNNumbers is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Chernoff E<lt>eric@pepper.weaverstreet.netE<gt>
+
+=cut
+
